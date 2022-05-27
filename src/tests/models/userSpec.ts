c48dba16ec4c9d47b71ee2adb86d386a979ece5e
AliTaarek <eng.alii.tarek@gmail.com>
2022-05-28 00:19:56 +0200

    modifications

--- src/tests/models/userSpec.ts
+++ src/tests/models/userSpec.ts
@@ -16,27 +16,4 @@ describe('User Model', () => {
     expect(testUser.update).toBeDefined()
   })
 
-  it('create method should create a new user', async () => {
-    const userData: user = {
-      firstName: 'John',
-      lastName: 'Doe',
-      userName: 'johndoe',
-      password: 'password',
-    }
-    const newUser = await testUser.create(userData)
-    expect(newUser.firstName).toBe('John')
-    expect(newUser.lastName).toBe('Doe')
-    expect(newUser.userName).toBe('johndoe')
-  })
-
-  it('index method should return all users', async () => {
-    const users: userUpdate[] = await testUser.index()
-    expect(users.length).toBe(1)
-    expect(users[0]).toEqual({
-      id: 1,
-      firstName: 'John',
-      lastName: 'Doe',
-      userName: 'johndoe',
-    })
-  })
 })
